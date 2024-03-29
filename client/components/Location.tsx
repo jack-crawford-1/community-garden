0e407bfd27b1a4e395543a3015bbc24863e426cd
Jack Crawford <primes_unit.05@icloud.com>
2024-03-30 10:54:55 +1300

    added map page with addLocation option using leaflet JS

---
+++ client/components/Location.tsx
@@ -0,0 +1,8 @@
+function Location() {
+  return (
+    <div>
+      <h2>Add Location</h2>
+    </div>
+  )
+}
+export default Location
--- client/router.tsx
+++ client/router.tsx
@@ -7,12 +7,16 @@ import {
 import App from './components/App'
 import AllUsers from './components/AllUsers'
 import Home from './components/Home'
+import MapPage from './components/MapPage'
+import Location from './components/Location'
 
 const router = createBrowserRouter(
   createRoutesFromElements([
     <Route path="/" element={<App />}>
       <Route index element={<Home />} />
       <Route path="/users/" element={<AllUsers />} />
+      <Route path="/map/" element={<MapPage />} />
+      <Route path="/location/" element={<Location />} />
     </Route>,
   ]),
 )

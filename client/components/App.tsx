0f6f674351e64414c9b5f3dfbd6a86dc792a6bb4
Jack Crawford <primes_unit.05@icloud.com>
2024-03-31 14:49:54 +1300

    styled to have fixed nav links

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,13 +1,37 @@
-import { Outlet } from 'react-router-dom'
-
+import { NavLink, Outlet } from 'react-router-dom'
 function App() {
   return (
-    <>
-      <div className="app">
-        <h1>Community Gardens Hub</h1>
+    <div className="app">
+      <div className="title-and-nav">
+        <NavLink className="nav-link home" to={'/'}>
+          <h1>Community Gardens Hub</h1>
+        </NavLink>
+        <ul className="navigation">
+          <li>
+            <NavLink className="nav-link" to={'/map'}>
+              Map
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="nav-link" to={'/sites'}>
+              Sites List
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="nav-link" to={'/users'}>
+              Users List
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="nav-link" to={'/councils'}>
+              Councils
+            </NavLink>
+          </li>
+          {/* Add additional links here if needed */}
+        </ul>
       </div>
       <Outlet />
-    </>
+    </div>
   )
 }
 
--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -1,5 +1,3 @@
-import { NavLink } from 'react-router-dom'
-
 function Home() {
   return (
     <div className="home-container">
@@ -7,52 +5,6 @@ function Home() {
         A web app to find and share potential locations and resources for
         community/ urban gardens.
       </p>
-      <ul>
-        <button className="nav-button">
-          <li>
-            <NavLink className="nav-link" to={'/users'}>
-              All Users
-            </NavLink>
-          </li>
-        </button>
-
-        <button className="nav-button">
-          <li>
-            <NavLink className="nav-link" to={'/map'}>
-              Map
-            </NavLink>
-          </li>
-        </button>
-
-        <button className="nav-button">
-          <li>
-            <NavLink className="nav-link" to={'/adduser'}>
-              Add User
-            </NavLink>
-          </li>
-        </button>
-        <button className="nav-button">
-          <li>
-            <NavLink className="nav-link" to={'/sites'}>
-              All Sites
-            </NavLink>
-          </li>
-        </button>
-        <button className="nav-button">
-          <li>
-            <NavLink className="nav-link" to={'/addsite'}>
-              Add Site
-            </NavLink>
-          </li>
-        </button>
-        <button className="nav-button">
-          <li>
-            <NavLink className="nav-link" to={'/councils/'}>
-              Councils
-            </NavLink>
-          </li>
-        </button>
-      </ul>
     </div>
   )
 }

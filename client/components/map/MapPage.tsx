6dfceadda30bf5e59288add84f97c862036e6ce3
Jack Crawford <primes_unit.05@icloud.com>
2024-04-02 09:58:20 +1300

    added overlay layer with custom icons to filter pre-determined sites

--- client/components/map/MapPage.tsx
+++ client/components/map/MapPage.tsx
@@ -1,63 +1,92 @@
 import { useEffect } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import L, { polygon } from 'leaflet'
 
 function MapPage() {
   const navigate = useNavigate()
 
+  // add new layer as const under below two and then call in OverLayers further down
   useEffect(() => {
-    const map = L.map('map').setView([-41.28664, 174.77557], 15)
+    const map = L.map('map').setView([-41.279519, 174.780011], 15)
 
-    const latlngs: L.LatLngExpression[] = [
+    const customIconProposedGarden = L.icon({
+      iconUrl:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjehet-5HTJwndzHzDbUgK15eYliTmlwJmPg&usqp=CAU',
+      iconSize: [38, 38],
+      iconAnchor: [22, 94],
+      popupAnchor: [-3, -76],
+    })
+    const landmarkLayer = L.marker([-41.279519, 174.780011], {
+      icon: customIconProposedGarden,
+    }).bindPopup('Seasonal Public Garden')
+
+    const customIcon = L.icon({
+      iconUrl: 'https://cdn-icons-png.flaticon.com/512/6593/6593775.png',
+      iconSize: [38, 38],
+      iconAnchor: [22, 94],
+      popupAnchor: [-3, -76],
+    })
+
+    const compost1 = L.marker([-41.306229, 174.779469], {
+      icon: customIcon,
+    }).bindPopup('Compost Drop Off point')
+
+    const compost2 = L.marker([-41.299014, 174.75938], {
+      icon: customIcon,
+    }).bindPopup('Compost Drop Off point')
+
+    const compost3 = L.marker([-41.239762, 174.782642], {
+      icon: customIcon,
+    }).bindPopup('Compost Drop Off point')
+
+    const compost4 = L.marker([-41.220487, 174.860075], {
+      icon: customIcon,
+    }).bindPopup('Compost Drop Off point')
+
+    // Placeholder location to show polygon on the map
+    // shows "const Wadestown" content onClick
+    const WadestownLatlngs: L.LatLngExpression[] = [
       [-41.265575, 174.780329],
       [-41.265905, 174.779937],
       [-41.26571, 174.779892],
       [-41.265574, 174.780073],
     ]
-    const polygon = L.polygon(latlngs, { color: 'blue' }).addTo(map)
+    const Wadestown = L.polygon(WadestownLatlngs, { color: 'blue' }).addTo(map)
       .bindPopup(`ID: 1 <br/>
       LatLong: 40.7128,-74.0060 <br/>
       Address: 12 Barnard St, Wadestown <br/>
-      Description: Community garden in Wadestown <br/>
-      Council ID: 1 <br/>
-      User ID: 1 <br/>
-      Public:Yes <br/>
-      Water Access:Yes <br/>
-      Available:Yes <br/>
-      Shade:No <br/>
-      Soil Type: Loamy <br/>
-      Size: Medium <br/>
-      Accessibility:Public transit`)
-    // map.fitBounds(polygon.getBounds())
+      Description: Community garden in Wadestown <br/>`)
 
-    const mtVicWellingtonSquare = [
-      [-41.2964, 174.7943],
-      [-41.2965, 174.7948],
-      [-41.2976, 174.7942],
-      [-41.2965, 174.7943],
-    ]
-    const square = L.polygon(mtVicWellingtonSquare, { color: 'red' }).addTo(map)
-      .bindPopup(`
-        latlong: '51.5074,-0.1278',<br/>
-        address: '456 Park Ln, Mt Vic',<br/>
-        description: 'Private garden space near Mt Vic Park',<br/>
-        councilId: 1,<br/>
-        userId: 2,<br/>
-        isPublic: false,<br/>
-        hasWaterAccess: true,<br/>
-        isAvailable: true,<br/>
-        hasShade: true,<br/>
-        soilType: 'Sandy',<br/>
-        size: 'Large',<br/>
-        accessibility: 'Street parking',
-      ,`)
-    // map.fitBounds(square.getBounds())
+    // Base layer map
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 20,
       attribution:
         '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     }).addTo(map)
+
+    const baseLayers = {
+      OpenStreetMap: L.tileLayer(
+        'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        {
+          maxZoom: 20,
+          attribution:
+            '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        },
+      ),
+    }
+
+    // created a compost layer so I could show all compost locations under one layer
+    const compostLayer = L.layerGroup([compost1, compost2, compost3, compost4])
+
+    // Can add more layers
+    const overlayLayers = {
+      ExistingSites: Wadestown,
+      ProposedSites: landmarkLayer,
+      CompostDrop: compostLayer,
+    }
+
+    L.control.layers(baseLayers, overlayLayers).addTo(map)
 
     const popup = L.popup()
 

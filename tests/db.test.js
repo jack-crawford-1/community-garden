9c791521815575b09a4b7c5d97f69d4d0ed79199
Jack Crawford <primes_unit.05@icloud.com>
2024-04-01 23:12:13 +1300

    staggered seed run to get  rid of foreign key issue seeding  but second test failing

--- tests/db.test.js
+++ tests/db.test.js
@@ -7,10 +7,14 @@ beforeAll(() => {
   return connection.migrate.latest()
 })
 
+// TODO Foreign Key issue
+//
+
 beforeEach(async () => {
-  await connection.seed.run({ specific: 'councils.js' })
-  await connection.seed.run({ specific: 'users.js' })
-  await connection.seed.run({ specific: 'sites.js' })
+  await connection.seed.run()
+  // await connection.seed.run({ specific: '01_councils.js' })
+  // await connection.seed.run({ specific: '02_users.js' })
+  // await connection.seed.run({ specific: '03_sites.js' })
 })
 
 describe('getAllUsers', () => {
@@ -20,6 +24,18 @@ describe('getAllUsers', () => {
   })
 })
 
+// Dont need the "specific" abve after adding num order to seed files but still failing deleteUser due to foreign key constraint - need to figure out how to remove dependencies
+//
+
+describe('deleteUser', () => {
+  it('should delete a user', async () => {
+    await db.deleteUser(1)
+    const users = await db.getAllUsers()
+    expect(users).toHaveLength(9)
+  })
+})
+
+// tried destrying the connection after each and using "specific" didnt work
 afterAll(() => {
   return connection.destroy()
 })
